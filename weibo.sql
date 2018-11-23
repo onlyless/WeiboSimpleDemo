8219d2483e4f4556d374cafee4464762f0fa71f5
onlyless <django_lf@163.com>
2018-11-23 13:03:07 +0800

    add mysql database

---
+++ weibo.sql
@@ -0,0 +1,102 @@
+-- MySQL dump 10.13  Distrib 5.7.10, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: weibo
+-- ------------------------------------------------------
+-- Server version	5.7.10
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message` (
+  `id` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `message` varchar(4000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES ('071foIrCuX','admin','2018-11-23 10:32:01','dsfd'),('123231jklaf','admin','2018-11-22 00:00:00','123test'),('12323231jklaf','admin','2018-11-22 13:28:30','123test'),('32231wjkaf','admin','2018-11-22 13:42:24','123test'),('9kaAf5B6yv','admin','2018-11-23 12:45:25','hest'),('BNCaajiYsS','admin','2018-11-23 12:45:37','hest'),('bPh8Gae14c','admin','2018-11-23 12:48:23','<b>hello</b>'),('KiAuC9ERlP','admin','2018-11-22 13:55:27','fad'),('MTGVcnAVHC','admin','2018-11-22 13:40:35','fsdzf'),('qEfeMTROO0','admin','2018-11-22 13:46:00','fds'),('sQo8ImPxtn','admin','2018-11-22 13:40:30','jklsadf'),('ZY1KYrCJoR','admin','2018-11-23 12:47:23','&lt;b&gt;hello&lt;/b&gt;');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `test`
+--
+
+DROP TABLE IF EXISTS `test`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test` (
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `test`
+--
+
+LOCK TABLES `test` WRITE;
+/*!40000 ALTER TABLE `test` DISABLE KEYS */;
+INSERT INTO `test` VALUES ('2018-08-12 00:00:00'),('2018-11-22 13:15:02'),('2018-11-22 13:15:28'),('2018-11-22 13:15:40');
+/*!40000 ALTER TABLE `test` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (-594824697110164410,'zz742962178@gmail.com','21312','null'),(-996228323,'zz742962178@gmail.com','root','null'),(21312,'vip@djangoz.com','vip','root'),(2267786627385084324,'zz742962178@gmail.com','aa','123456'),(2521794780550734734,'zz742962178@gmail.com','admin','123456a');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-23 13:00:01
